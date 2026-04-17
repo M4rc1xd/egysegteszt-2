05c0ce7281a9e7e24d85f053af1dc357825373cd
Kover Marcell <1228marci@google.com>
2026-04-17 12:15:55 +0200

    veletlen tok mas projektet csinaltam ugyhogy ujba ment az elozo ugyan ez csak a vegen nincs "2"

---
+++ classes/EmployeeStatistics.ts
@@ -0,0 +1,68 @@
+export interface Employee {
+	name: string;
+	age: number;
+	salary: number;
+}
+
+export class EmployeeStatistics {
+	employee: Employee[];
+
+	constructor(employee: Employee[]) {
+		if (!employee || employee.length === 0) {
+			throw new Error("A dolgozók megadása kötelező");
+		}
+
+		this.employee = employee;
+	};
+
+	getMaxSalary(): number | undefined {
+        let maxSalary: number | undefined = this.employee[0]?.salary;
+        if (typeof maxSalary != "number") {
+            throw new Error(`A 'getMaxSalary' nem tudott lefutni, a 'salary' valtozo tipusa: ${typeof maxSalary}`);
+        } else {
+            this.employee.forEach((emp: Employee) => {
+                if (emp.salary > maxSalary) {
+                    maxSalary = emp.salary;
+                };
+            });
+            return maxSalary;
+        };
+    };
+
+	getAvarageAge(): number | undefined {
+        let summedAges: number = 0;
+        if (typeof this.employee[0]?.age != "number") {
+            throw new Error(`A 'getAvarageAge' nem tudott lefutni, az 'age' valtozo tipusa: ${typeof this.employee[0]?.age}`);
+        } else {
+            this.employee.forEach((emp: Employee) => {
+                summedAges += emp.age;
+            });
+            const avgAge = (summedAges / (this.employee.length));
+            return Math.round(avgAge);
+        };
+    };
+
+	    getHighestPaidEmployee(): Employee | undefined {
+        let highestPaidEmployee: Employee | undefined = this.employee[0];
+        if (typeof highestPaidEmployee != "object") {
+            throw new Error(`A 'getHighestPaidEmployee' nem tudott lefutni, az 'Employee' tipusa: ${typeof highestPaidEmployee}`);
+        } else {
+            this.employee.forEach((emp: Employee) => {
+                if (emp.salary > highestPaidEmployee.salary) {
+                    highestPaidEmployee = emp;
+                };
+            });
+            return highestPaidEmployee;
+        };
+    };
+	
+	countEmployeesOverSalary(salary: number): number {
+        let employeesOverSalary: number = 0;
+        this.employee.forEach((emp: Employee) => {
+            if(emp.salary > salary) {
+                employeesOverSalary ++;
+            };
+        });
+        return employeesOverSalary;
+    };
+}
